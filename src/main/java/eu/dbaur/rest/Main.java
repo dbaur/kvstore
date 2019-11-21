4bf4ca544292f40e03dcad0223289ab6bf856e0f
Daniel Baur <daniel.baur@uni-ulm.de>
2019-11-21 10:44:59 +0100

    shutdown on jvm down

--- src/main/java/eu/dbaur/rest/Main.java
+++ src/main/java/eu/dbaur/rest/Main.java
@@ -34,10 +34,13 @@ public class Main {
    */
   public static void main(String[] args) throws IOException {
     final HttpServer server = startServer();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(
+        server::shutdown
+    ));
+
     System.out.println(String.format("Jersey app started with WADL available at "
-        + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-    System.in.read();
-    server.stop();
+        + "%sapplication.wadl", BASE_URI));
   }
 }
 
